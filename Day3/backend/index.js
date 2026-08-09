c948caff307c1288d08d7d6a3428d4e2ce495cdd
Hardhikshettyr <hardhikshetty1771@gmail.com>
2026-08-09 19:20:13 +0530

    .

---
+++ Day3/backend/index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+const port=3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get("/register",(req,res)=>{
+    res.send("Standard get response");
+})
+app.post("/register",(req,res)=>{
+    console.log(req.body)
+    res.send("Standard post response");
+    
+})
+
+app.listen (port,()=>{
+    console.log("listing ")
+})
